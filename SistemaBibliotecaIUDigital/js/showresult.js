bd3b862675504d91738d8f3f8eb1686a18e8ebe1
hdgarcia <hdgarcia@bancolombia.com.co>
2023-09-14 20:15:17 -0500

    update

--- SistemaBibliotecaIUDigital/js/showresult.js
+++
@@ -1,8 +0,0 @@
-var showButton = document.getElementById('showResult');
-var resultsDiv = document.getElementById('resultData');
-
-showButton.addEventListener('click', function(e) {
-    e.preventDefault();
-    resultsDiv.style.display = 'block';
-});
-
